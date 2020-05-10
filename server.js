85436a914c7b8245369ff60f47fba749912d115b
ks602 <andyjack0602@yahoo.com.hk>
2020-05-10 17:32:50 +0800

    routing complete

--- server.js
+++ server.js
@@ -1,20 +1,35 @@
 let express = require("express");
+let bodyParser = require("body-parser");
 let app = express();
 
-app.use(express.static(__dirname + "/client"));
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(bodyParser.json());
 
-app.listen(9001, () => {
-  console.log("Listening on port 9001!");
+app.use(express.static(__dirname + "/client"));
+app.use("/css", express.static(__dirname + "client/css"));
+app.use("/js", express.static(__dirname + "client/js"));
+
+app.listen(9007, () => {
+  console.log("Listening on port 9007!");
 });
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/client/index.html");
 });
 
+app.post("/sendLogin", (req, res) => {
+  console.log(req.body["username"]);
+  res.json({
+    status: false
+  });
+})
+
 app.get("/game", (req, res) => {
   res.sendFile(__dirname + "/client/game.html");
 });
 
 app.get("*", (req, res) => {
-  res.sendFiLe(__dirname + "/client/404.html");
+  res.status(404).sendFile(__dirname + "/client/404.html");
 });
